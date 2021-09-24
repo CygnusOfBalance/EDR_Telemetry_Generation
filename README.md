94f58f91d46f1de3cef82e259be4b522de89179d
Noah Hansen <noah.hansen1323@gmail.com>
2021-09-24 09:23:17 -0400

    minor changes to error handling

--- README.md
+++ README.md
@@ -1,19 +1,2 @@
 # EDR_Telemetry_Generation
 This python script is designed to generate telemetry in which can be tracked by Red Canary's EDR tool.
-
-How to use:
-     python telegen.py <option1> <argument1> <option2> <argument2> ....
-
-Command Line Options:
-     -h, --help
-          show this help message and exit
-     --startprocess <process>
-          start a process/shell command
-     --createfile <file_path>
-          create a file at the specified location
-     --modfile <file_path>
-          modify a file at the specified location
-     --deletefile <file_path>
-          delete a file at the specified location
-     --startconnection     
-          start a connection with google.com and transmit data
--- telegen.py
+++ telegen.py
@@ -15,9 +15,14 @@ from subprocess import Popen, PIPE
 ### logData ###
 ### Method for writing data to our specified log ###
 def logData(log_path, elements):
-    with open(log_path, 'a+', newline='') as fileObj:
-        csvWriter = writer(fileObj)
-        csvWriter.writerow(elements)
+    try:
+        with open(log_path, 'a+', newline='') as fileObj:
+            csvWriter = writer(fileObj)
+            csvWriter.writerow(elements)
+    except Exception as a:
+        print("ERROR: There was an error attempting to write to log " + log_path)
+        print("Exception: ")
+        print(a)
 
 ### aggregrateFileData ###
 ### Method for aggregating all log data regarding file mods, creation, and deletion ###
@@ -46,8 +51,6 @@ def aggregrateFileData(activity_arg, filePath):
 def startProcess(command):
     print("Starting Process: " + str(command))
     commandArr = command.split(" ")
-    #process = os.system(command)
-    #print(sys.platform)
     process = Popen(commandArr, stdout=PIPE, stdin=PIPE, shell=True)
 
     ### this has to be done so the command doesn't throw a write error
@@ -74,12 +77,22 @@ def startProcess(command):
 ### createFile ###
 ### method for creating a file at the specified path ###
 def createFile(filePath):
+
+    ### Checks if file exists
     if os.path.exists(filePath):
         print("ERROR: This file already exists")
         print("Cancelling file creation...")
         return None
 
-    f=open(filePath, "w+")
+    ### Error handles any other problems
+    try:
+        f=open(filePath, "a+")
+    except Exception as a:
+        print("ERROR: There was an error attempting to modify the file at " + filePath)
+        print("Cancelling file modification...")
+        print("Exception: ")
+        print(a)
+        return None
     print("File has been created at " + filePath)
 
     ### Aggregate logging info
@@ -91,12 +104,22 @@ def createFile(filePath):
 ### method for modifying a file at the specified path ###
 def modifyFile(filePath):
 
+    ### Checks if file doesn't exist
     if not os.path.exists(filePath):
         print("ERROR: This file doesn't exist exists")
         print("Cancelling file modification...")
         return None
 
-    f=open(filePath, "a+")
+    ### Error handles any other problems
+    try:
+        f=open(filePath, "a+")
+    except Exception as a:
+        print("ERROR: There was an error attempting to modify the file at " + filePath)
+        print("Cancelling file modification...")
+        print("Exception: ")
+        print(a)
+        return None
+
     f.write("THIS IS A FILE MODIFICATION")
     print("File has been modified at " + filePath)
 
@@ -110,9 +133,11 @@ def modifyFile(filePath):
 def deleteFile(filePath):
     try:
         os.remove(filePath)
-    except Exception:
-        print("ERROR: File does not exist at " + filePath)
+    except Exception as a:
+        print("ERROR: There was an error trying to remove the file at " + filePath)
         print("Cancelling file deletion...")
+        print("Exception: ")
+        print(a)
         return None
 
     print("File has been removed at " + filePath)
@@ -134,9 +159,11 @@ def establishConnection():
         s.send(request)
 
         print("Request has been sent")
-    except Exception:
+    except Exception as a:
         print("ERROR: there was an error while connecting")
         print("Cancelling connection...")
+        print("Exception: ")
+        print(a)
         return None
 
 
@@ -176,7 +203,7 @@ if __name__ == "__main__":
     ### argparse is used to create command line arguments for the framework
     parser = argparse.ArgumentParser(description='Red Canary Telemetry Generation')
 
-    parser.add_argument('--startprocess', metavar='<process>', type=str,
+    parser.add_argument('--startprocess', metavar='\"<process>\"', type=str,
                         help="start a process/shell command", required=False)
 
     parser.add_argument('--createfile', metavar='<file_path>', type=str,
@@ -194,22 +221,16 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    startprocess_arg = args.startprocess
-    createfile_arg = args.createfile
-    modfile_arg = args.modfile
-    deletefile_arg = args.deletefile
-    startconnection_arg = args.startconnection
-
     ### Would prefer to use a switch statement here.. but python doesn't support that ###
-    if startprocess_arg:
-        startProcess(startprocess_arg)
-    if createfile_arg:
-        createFile(createfile_arg)
-    if modfile_arg:
-        modifyFile(modfile_arg)
-    if deletefile_arg:
-        deleteFile(deletefile_arg)
-    if startconnection_arg:
+    if args.startprocess:
+        startProcess(args.startprocess)
+    if args.createfile:
+        createFile(args.createfile)
+    if args.modfile:
+        modifyFile(args.modfile)
+    if args.deletefile:
+        deleteFile(args.deletefile)
+    if args.startconnection:
         establishConnection()
-    if not (startprocess_arg or createfile_arg or modfile_arg or deletefile_arg or startconnection_arg):
+    if not (args.startprocess or args.createfile or args.modfile or args.deletefile or args.startconnection):
         print(args)
